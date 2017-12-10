8dda37598793fa17c34d2c4f92f478741dabff89
Andhie <andhie@agmostudio.com>
2017-12-10 16:56:58 +0800

    4-Firestore Post

--- PasarMalam/app/src/main/java/com/agmostudio/pasarmalam/MainActivity.java
+++ PasarMalam/app/src/main/java/com/agmostudio/pasarmalam/MainActivity.java
@@ -1,18 +1,32 @@
 package com.agmostudio.pasarmalam;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -67,5 +81,66 @@ public class MainActivity extends AppCompatActivity {
         };
 
         recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // add our Suggesstion to the given menu
+        getMenuInflater().inflate(R.menu.menu_suggest, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_suggest) {
+            showSuggestionDialog();
+            return true; // tell others we have handled it
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showSuggestionDialog() {
+        View dialogLayout = LayoutInflater.from(this).inflate(R.layout.dialog_suggest, null);
+        final EditText suggestText = dialogLayout.findViewById(R.id.suggestion);
+
+        new AlertDialog.Builder(this)
+                .setTitle("Suggest a Location")
+                .setView(dialogLayout)
+                .setNegativeButton("Cancel", null)
+                .setPositiveButton("Suggest", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String text = suggestText.getText().toString().trim();
+
+                        if (TextUtils.isEmpty(text)) {
+                            // user didnt type anything, just exit
+                            return;
+                        }
+
+                        Map<String, Object> data = new HashMap<>();
+                        data.put("name", text);
+
+                        FirebaseFirestore.getInstance()
+                                .collection("suggestions")
+                                .add(data)
+                                .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<DocumentReference> task) {
+                                        String msg;
+                                        if (task.isSuccessful()) {
+                                            msg = "Thank you for your suggestion";
+                                        } else {
+                                            msg = task.getException().getMessage();
+                                        }
+
+                                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+
+                                    }
+                                });
+
+                    }
+                })
+                .show();
+
     }
 }
